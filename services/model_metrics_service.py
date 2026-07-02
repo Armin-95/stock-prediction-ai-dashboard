45c74c8f4bb14fe47063257f6113de4b8681044f
Armin <mednansky.armin@gmail.com>
2026-07-02 13:45:33 +0200

    Clean nested model metrics for AI prompts - Remove None values from nested metric dictionaries - Skiped empty or missing model metric entries

--- services/model_metrics_service.py
+++ services/model_metrics_service.py
@@ -21,10 +21,41 @@ def get_all_model_metrics_for_symbol(symbol: str, models: dict):
     
     return model_metrics
 
+
 #clear None values for AI explanation
 def _filter_available_metrics(model_metrics: dict | None):
-    return {
-        model_type: metrics
-        for model_type, metrics in model_metrics.items()
-        if metrics is not None
-    }
+    if not model_metrics:
+        logger.error("No model metrics has been sent.")
+        return {}
+    
+    cleaned_model_metrics = {}
+    for model_type, metric_type in model_metrics.items():
+        if not model_type:
+            continue
+
+        cleaned_metrics={}
+        if metric_type:
+
+            for metric, value in metric_type.items():
+                if value is not None:
+                  cleaned_metrics[metric]  = value
+
+        if cleaned_metrics:
+            cleaned_model_metrics[model_type] = cleaned_metrics 
+
+    return cleaned_model_metrics
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
